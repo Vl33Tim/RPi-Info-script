1ce1e482a4355dc74820f765a5ccc9f060c903a4
Vl33Tim <46729952+Vl33Tim@users.noreply.github.com>
2021-05-19 21:01:27 +0300

    Update tginfostartup.sh

--- tginfostartup.sh
+++ tginfostartup.sh
@@ -9,8 +9,8 @@ string3=$(date +'%d/%m/%Y')
 MESSAGE=$string1$string2"%20%20"$string3
 # Print the third string by combining with other string
 echo "$MESSAGE to our site"
-TOKEN=1561203252:AAHleGjKi26GetGErR1RJJQEr0j5iCSOe5k
-CHAT_ID=-455755082
+TOKEN=
+CHAT_ID=
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
